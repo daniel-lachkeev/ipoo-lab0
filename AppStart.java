03865d92bd922dc309ef35d22f17b588b6bf4a01
Daniel Lachkeev <daniel.lachkeev@protonmail.com>
2021-03-04 17:18:43 +0000

    small fix

--- AppStart.java
+++ AppStart.java
@@ -3,7 +3,7 @@ public class AppStart {
         Size size = Size.MEDIUM;
         System.out.println("Name:\t\t" + " " + size.name());
         System.out.println("toString():\t" + " " + size.toString());
-        System.out.println("Ordem:\t\t" + " " + 1);
+        System.out.println("Ordem:\t\t" + " " + size.ordinal());
         System.out.println("Valor mínimo:\t" + " " + size.getMinValue());
         System.out.println("Valor máximo:\t" + " " + size.getMaxValue());
         System.out.println("Código:\t\t" + " " + size.getCode());
